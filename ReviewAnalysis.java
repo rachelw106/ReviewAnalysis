28ea9590176dd715a591ee15f5134688f32b2cb1
rachelw106 <rachelw106@nycstudents.net>
2025-03-05 14:30:04 +0000

    commit

---
+++ ReviewAnalysis.java
@@ -0,0 +1,33 @@
+public class ReviewAnalysis {
+    /** All user reviews to be included in this analysis */
+    private Review[] allReviews;
+
+    /**
+     * Initializes allReviews to contain all the Review objects to be
+     * analyzed
+     */
+    public ReviewAnalysis() {
+        /* implementation not shown */ }
+
+    /**
+     * Returns a double representing the average rating of all the Review
+     * objects to
+     * be
+     * analyzed, as described in part (a)
+     * Precondition: allReviews contains at least one Review.
+     * No element of allReviews is null.
+     */
+    public double getAverageRating() {
+        /* to be implemented in part (a) */ }
+
+    /**
+     * Returns an ArrayList of String objects containing formatted
+     * versions of
+     * selected user comments, as described in part (b)
+     * Precondition: allReviews contains at least one Review.
+     * No element of allReviews is null.
+     * Postcondition: allReviews is unchanged.
+     */
+    public ArrayList<String> collectComments() {
+        /* to be implemented in part (b) */ }
+}
